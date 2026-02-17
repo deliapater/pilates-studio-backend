76fc1a39f7138c103d28ffd004a1385b056ced88
Delia Paternina <delia.paternina@gmail.com>
2026-02-17 11:46:52 +0000

    adds is_admin logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true
         ]);
+        User::factory(5)->create();
         $this->call(ClassesTableSeeder::class);
     }
 }
